3652f0bce3321e8373f9f5a49166def11617d9a1
Youssef Nawar <youssef.nawar10@yahoo.com>
2020-04-30 01:59:25 +0200

    Save and Load

---
+++ src/Logic/GameLevels/LevelState.java
@@ -0,0 +1,9 @@
+package Logic.GameLevels;
+
+import Logic.Model;
+
+public class LevelState {
+    private Level level;
+    private Model model;
+
+}
--- src/animation/Projector.java
+++ src/animation/Projector.java
@@ -29,7 +29,7 @@ public class Projector {
 //        });
     }
 
-    public void generateTimeLine() {
+    public Projector() {
 
     }
 
